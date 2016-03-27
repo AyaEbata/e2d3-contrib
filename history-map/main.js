37a66d5044a19e2374f65b9d5911ae640f56737c
えばたあや <ayaaaaa.0u0w@gmail.com>
2016-03-27 18:19:58 +0900

    中心地を設定

--- history-map/main.js
+++ history-map/main.js
@@ -8,9 +8,9 @@ d3.select('#mapid')
   .style('height', root.clientHeight + 'px')
 
 function update(data) {
-    // [緯度, 経度], ズーム
-    var mymap = L.map('mapid').setView([51.505, -0.09], 13);
-    
+    // しぇあひるずを中心に表示  setView([緯度, 経度], ズーム)
+    var mymap = L.map('mapid').setView([35.472020834262, 139.62539970875], 15);
+
     L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="http://osm.org/copyright" target="_blank">OpenStreetMap</a> contributors',
     }).addTo(mymap);
