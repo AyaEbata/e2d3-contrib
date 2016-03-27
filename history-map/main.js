adb287313f05e041b8ffc092d7c1bca84d15ee4c
えばたあや <ayaaaaa.0u0w@gmail.com>
2016-03-27 18:26:02 +0900

    文言を修正

--- history-map/main.js
+++ history-map/main.js
@@ -2,16 +2,16 @@
 
 d3.select(root)
   .append('div')
-  .attr('id', 'mapid')
+  .attr('id', 'map-container')
 
-d3.select('#mapid')
+d3.select('#map-container')
   .style('height', root.clientHeight + 'px')
 
 function update(data) {
     // しぇあひるずを中心に表示  setView([緯度, 経度], ズーム)
-    var mymap = L.map('mapid').setView([35.472020834262, 139.62539970875], 15);
+    var mapLayer = L.map('map-container').setView([35.472020834262, 139.62539970875], 15);
 
     L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="http://osm.org/copyright" target="_blank">OpenStreetMap</a> contributors',
-    }).addTo(mymap);
+    }).addTo(mapLayer);
 }
