7f6e403e6306aa8b7288ae148e4f5c32b378dc5f
えばたあや <ayaaaaa.0u0w@gmail.com>
2016-03-29 14:42:03 +0900

    案２なんとなくできた

--- history-map2/main.js
+++ history-map2/main.js
@@ -25,21 +25,58 @@ function update(data) {
           .addTo(mapLayer);
 
         popup.on('click', function() {
-            d3.select(root)
+            var modalContent = d3.select(root)
               .append('div')
               .attr('id', 'modal-content')
               .style({
-                  'width': root.clientWidth - 100 + 'px',
-                  'height': root.clientHeight - 100 + 'px'
+                  'width': root.clientWidth - 160 + 'px',
+                  'height': root.clientHeight - 160 + 'px'
               })
+
+            modalContent
+              .append('div')
+              .attr('id', 'spot')
               .text(spot[data[0][0]])
+
+            modalContent
+              .append('div')
+              .attr('id', 'description')
+              .text(spot[data[0][2]])
+
+            modalContent
+              .append('div')
+              .attr('id', 'address')
+              .text(spot[data[0][1]])
+
+            var pictContainerWidth = (root.clientWidth - 180) * 0.5 + 'px';
+            var pictContainerHeight = root.clientHeight - document.getElementById('spot').clientHeight - document.getElementById('description').clientHeight - document.getElementById('address').clientHeight - 190 + 'px';
+
+            modalContent
+              .append('img')
+              .attr('id', 'old-picture')
+              .attr('src', spot[data[0][3]])
+              .style({
+                  'max-width': pictContainerWidth,
+                  'max-height': pictContainerHeight,
+                  'margin-right': '20px'
+              })
+
+            modalContent
+              .append('img')
+              .attr('id', 'now-picture')
+              .attr('src', spot[data[0][4]])
+              .style({
+                  'max-width': pictContainerWidth,
+                  'max-height': pictContainerHeight
+              })
+
+            modalContent
               .append('div')
               .attr('id', 'close-button')
               .text('×')
               .on('click', function() {
                   d3.select('#modal-content')
                     .remove()
-
                   d3.select('#modal-overlay')
                     .remove()
               })
