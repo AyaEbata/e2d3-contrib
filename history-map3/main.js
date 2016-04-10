48083d64b76d09d172e10fe763310acdf74802f4
えばたあや <ayaaaaa.0u0w@gmail.com>
2016-04-10 19:35:58 +0900

    ちょっときれいにした

--- history-map3/main.js
+++ history-map3/main.js
@@ -86,10 +86,10 @@ function createContents(spot, data, spotName) {
     var pictContainerWidth = (root.clientWidth - (modalPadding * 2) - pictMargin) * 0.5;
     var pictContainerHeight = root.clientHeight - document.getElementById('spot').clientHeight - document.getElementById('description').clientHeight - document.getElementById('address').clientHeight - 50 - (modalPadding * 2);
 
-    var magnification = 1;
-    if (spot[PICT3_LABEL] !== '' && spot[PICT4_LABEL] !== '') {
-        magnification = 0.75;
-    }
+    // var magnification = 1;
+    // if (spot[PICT3_LABEL] !== '' && spot[PICT4_LABEL] !== '') {
+    //     magnification = 0.75;
+    // }
 
     var p1 = function() {
         return new Promise(function(resolve) {
@@ -99,7 +99,7 @@ function createContents(spot, data, spotName) {
               .attr('src', spot[PICT1_LABEL])
               .style({
                   'max-width': pictContainerWidth + 'px',
-                  'max-height': pictContainerHeight * magnification + 'px',
+                  'max-height': pictContainerHeight + 'px',
                   'margin-right': pictMargin + 'px',
               })
               .on('click', function() {
@@ -109,7 +109,7 @@ function createContents(spot, data, spotName) {
                   resolve(getPictSize(this));
               })
         })
-    };
+    }
 
     var p2 = function() {
         return new Promise(function(resolve) {
@@ -119,7 +119,7 @@ function createContents(spot, data, spotName) {
               .attr('src', spot[PICT2_LABEL])
               .style({
                   'max-width': pictContainerWidth + 'px',
-                  'max-height': pictContainerHeight * magnification + 'px'
+                  'max-height': pictContainerHeight + 'px'
               })
               .on('click', function() {
                   createPictPreview(spot[PICT2_LABEL]);
@@ -128,58 +128,69 @@ function createContents(spot, data, spotName) {
                   resolve(getPictSize(this));
               })
         })
-    };
+    }
 
-    var getPictSize = function(this) {
+    var getPictSize = function(self) {
         var pictSize = { };
-        pictSize.width = parseFloat(d3.select(this).style('width'));
-        pictSize.height = parseFloat(d3.select(this).style('height'));
+        pictSize.width = parseFloat(d3.select(self).style('width'));
+        pictSize.height = parseFloat(d3.select(self).style('height'));
         return pictSize;          
     }
 
-    var pictBox = modalContent
-      .append('div')
-      .attr('id', 'pict-box')
-
-    pictBox
-      .append('img')
-      .attr('id', 'picture3')
-      .attr('src', spot[PICT3_LABEL])
-      .style({
-          'max-width': pictContainerWidth + 'px',
-          'max-height': pictContainerHeight * 0.25 + 'px',
-          'margin-right': pictMargin + 'px',
-          'margin-top': '10px'
-      })
-      .on('click', function() {
-          createPictPreview(spot[PICT3_LABEL]);
-      })
-
-    pictBox
-      .append('img')
-      .attr('id', 'picture4')
-      .attr('src', spot[PICT4_LABEL])
-      .style({
-          'max-width': pictContainerWidth + 'px',
-          'max-height': pictContainerHeight * 0.25 + 'px',
-          'margin-top': '10px'
-      })
-      .on('click', function() {
-          createPictPreview(spot[PICT4_LABEL]);
-      })
-
     var tasks = [ p1(), p2() ];
     Promise.all(tasks).then(function(results) {
-        if (isPictHeightsMax(results)) {
 
+        var pictBox = modalContent
+          .append('div')
+          .attr('id', 'pict-box')
+
+        showPicture3(pictBox);
+        showPicture4(pictBox);
+
+        if (isPictWidthsMax(results)) {
+            d3.select('#picture1')
+              .style('max-height', pictContainerHeight * 0.75 + 'px')
+
+            d3.select('#picture2')
+              .style('max-height', pictContainerHeight * 0.75 + 'px')
         }
-        console.log(results);
-    });
+    })
 
-  function isPictHeightsMax(results) {
-      return results[0].height == pictContainerHeight * magnification 
-              && results[1].height == pictContainerHeight * magnification;
-  }
+    var showPicture3 = function(pictBox) {
+        pictBox
+          .append('img')
+          .attr('id', 'picture3')
+          .attr('src', spot[PICT3_LABEL])
+          .style({
+              'max-width': pictContainerWidth + 'px',
+              'max-height': pictContainerHeight * 0.25 + 'px',
+              'margin-right': pictMargin + 'px',
+              'margin-top': '10px'
+          })
+          .on('click', function() {
+              createPictPreview(spot[PICT3_LABEL]);
+          })
+    }
+
+    var showPicture4 = function(pictBox) {
+        pictBox
+          .append('img')
+          .attr('id', 'picture4')
+          .attr('src', spot[PICT4_LABEL])
+          .style({
+              'max-width': pictContainerWidth + 'px',
+              'max-height': pictContainerHeight * 0.25 + 'px',
+              'margin-top': '10px'
+          })
+          .on('click', function() {
+              createPictPreview(spot[PICT4_LABEL]);
+          })
+    }
+
+    var isPictWidthsMax = function(pictArray) {
+        return pictArray[0].width == pictContainerWidth 
+            && pictArray[1].width == pictContainerWidth;
+    }
 
     modalContent
       .append('div')
